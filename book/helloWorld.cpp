171e501c6ee25640dd596ea21ca5a0e97bdb1075
Matt <MattDraft@gmail.com>
2016-12-24 00:11:13 -0600

    This should do it

---
+++ book/helloWorld.cpp
@@ -0,0 +1,10 @@
+//The classics!
+
+#include <iostream>
+
+int main(){
+
+	std::cout << "Hello World!\n"; //output "Hello World!"
+	return 0;
+
+}
---
+++ book/inputs.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // cout and more
+#include <string>   // Strings
+using namespace std;
+
+
+//cin takes input
+
+int main(){
+
+	string first_name;
+	int age;
+	cout << "Please enter your name and age: \n ";
+	cin >> first_name >> age;
+	cout << "So you are" << first_name << "and you are "<< age << " years old!\n";
+
+
+}
+
